265eebd523114ed7a14535667f6e63b775fa8f1a
Yuvraj Rathi <yrathi66@gmail.com>
2024-02-03 17:38:41 +0530

    lol

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 from _thread import * # this is for threading
 
-server = ""
+server = "172.16.96.243"
 port = 5555
 
 # these are types of connections
@@ -17,8 +17,25 @@ s.listen(2) # how many clients (listeners) do you
 print("Waiting for a connection, Server Started")
 
 def threaded_clients(conn):
-    pass
-
+    
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048) # how much data do you want to receive
+            reply = data.decode("utf-8")
+            
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received:", reply)
+                print("Sending:", reply)
+                
+            conn.sendall(str.encode(reply)) #encoding the data before sending it
+        except:
+            break;
+            
+        
 while True:
     conn, addr = s.accept() # conn is the connection, addr is the ip address
     print("Connected to:", addr)
